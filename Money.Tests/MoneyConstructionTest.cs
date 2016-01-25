7d89a70983454f909019584ade6db410eaec7a3f
zpbappi <zpbappi@gmail.com>
2016-01-25 15:42:41 +1100

    added money construction tests and implementation

---
+++ Money.Tests/MoneyConstructionTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xunit;
+using Shouldly;
+
+
+namespace Money.Tests
+{
+    public class MoneyConstructionTest
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void ShouldThrowWithEmptyCurrency(string currency)
+        {
+            Should.Throw<ArgumentNullException>(() => Money.Create(42, currency));
+        }
+
+        [Fact]
+        public void ShouldThrowWithNullValues()
+        {
+            Should.Throw<ArgumentNullException>(() => Money<decimal?>.Create(null, "AUD"));
+        }
+
+        [Theory]
+        [InlineData(42, "AUD")]
+        public void ShouldNotThrowWithProperValues(decimal amount, string currency)
+        {
+            Should.NotThrow(() => Money.Create(amount, currency));
+        }
+
+        [Theory]
+        [InlineData("aud", "AUD")]
+        [InlineData("aUd", "AuD")]
+        [InlineData("Aud", "AUD")]
+        public void CurrencyCodeShouldBeCaseIgnorant(string currencyCase1, string currencyCase2)
+        {
+            var money1 = Money.Create(42, currencyCase1);
+            var money2 = Money.Create(42, currencyCase2);
+
+            money1.Currency.ShouldBe(money2.Currency);
+        }
+    }
+}
---
+++ Money/Money.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Money
+{
+    public class Money<T>
+    {
+        public T Amount { get; private set; }
+        public string Currency { get; private set; }
+
+        protected Money(T amount, string currency)
+        {
+            if (IsNullable<T>() && amount == null)
+                throw new ArgumentNullException("amount");
+
+            if(string.IsNullOrWhiteSpace(currency))
+                throw new ArgumentNullException("currency");
+
+            this.Amount = amount;
+            this.Currency = currency.ToUpperInvariant();
+        }
+
+        public static Money<T> Create(T amount, string currency)
+        {
+            return new Money<T>(amount, currency);
+        }
+
+        public static Money<T> CreateWithCurrentCulterCurrency(T amount)
+        {
+            var currencyFromCurrentCulter =
+                new System.Globalization.RegionInfo(System.Threading.Thread.CurrentThread.CurrentUICulture.LCID)
+                    .ISOCurrencySymbol;
+            return Create(amount, currencyFromCurrentCulter);
+        }
+
+        private static bool IsNullable<TAmount>()
+        {
+            var type = typeof(TAmount);
+            
+            if (!type.IsValueType) 
+                return true; // reference type, definately nullable
+            
+            if (Nullable.GetUnderlyingType(type) != null) 
+                return true; // Nullable<T>
+            
+            return false; // value-type
+        }
+    }
+
+    public abstract class Money : Money<decimal>
+    {
+        protected Money(decimal amount, string currency) : base(amount, currency)
+        {
+        }
+    }
+}
