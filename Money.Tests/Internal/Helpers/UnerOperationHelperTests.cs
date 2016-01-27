115b23e21912cf2efb4561c384f8bcd0f2333c46
zpbappi <zpbappi@gmail.com>
2016-01-28 09:12:42 +1100

    added unary operation type helper for numeric values

---
+++ Money.Tests/Internal/Helpers/UnerOperationHelperTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Numerics;
+using Money.Internal.Helpers;
+using Shouldly;
+using Xunit;
+
+namespace Money.Tests.Internal.Helpers
+{
+    public class UnerOperationHelperTests
+    {
+        [Fact]
+        public void UnaryOperationOnIntShouldBeSupported()
+        {
+            EnsureIncrementDecrementNegation<int>(true);
+            UnaryOperationHelper.SupportsOnesCompliment<int>().ShouldBeTrue();
+        }
+
+        [Fact]
+        public void UnaryOperationOnLongShouldBeSupported()
+        {
+            EnsureIncrementDecrementNegation<long>(true);
+            UnaryOperationHelper.SupportsOnesCompliment<long>().ShouldBeTrue();
+        }
+
+        [Fact]
+        public void UnaryOperationOnShortShouldBeSupported()
+        {
+            EnsureIncrementDecrementNegation<short>(true);
+            UnaryOperationHelper.SupportsOnesCompliment<short>().ShouldBeTrue();
+        }
+
+        [Fact]
+        public void UnaryOperationOnDecimalShouldBeSupported()
+        {
+            EnsureIncrementDecrementNegation<decimal>(true);
+            UnaryOperationHelper.SupportsOnesCompliment<decimal>().ShouldBeFalse();
+        }
+
+        [Fact]
+        public void UnaryOperationOnBigIntegerShouldBeSupported()
+        {
+            EnsureIncrementDecrementNegation<BigInteger>(true);
+            UnaryOperationHelper.SupportsOnesCompliment<BigInteger>().ShouldBeTrue();
+        }
+
+        [Fact]
+        public void UnaryOperationOnInvalidTypeShouldNotBeSupported()
+        {
+            EnsureIncrementDecrementNegation<object>(false);
+            UnaryOperationHelper.SupportsOnesCompliment<object>().ShouldBeFalse();
+        }
+
+        [Fact]
+        public void ShouldProperlyDetectPartialUnaryOperationImplementation()
+        {
+            UnaryOperationHelper.SupportsIncrement<PartiallyUnarySupportedNumber>().ShouldBeTrue();
+            UnaryOperationHelper.SupportsDecrement<PartiallyUnarySupportedNumber>().ShouldBeFalse();
+            UnaryOperationHelper.SupportsNegation<PartiallyUnarySupportedNumber>().ShouldBeFalse();
+            UnaryOperationHelper.SupportsOnesCompliment<PartiallyUnarySupportedNumber>().ShouldBeTrue();
+            UnaryOperationHelper.SupportsUnaryPlus<PartiallyUnarySupportedNumber>().ShouldBeFalse();
+        }
+
+        public static void EnsureIncrementDecrementNegation<T>(bool expectedValue)
+        {
+            UnaryOperationHelper.SupportsIncrement<T>().ShouldBe(expectedValue);
+            UnaryOperationHelper.SupportsDecrement<T>().ShouldBe(expectedValue);
+            UnaryOperationHelper.SupportsNegation<T>().ShouldBe(expectedValue);
+            UnaryOperationHelper.SupportsIncrement<T>().ShouldBe(expectedValue);
+        }
+    }
+
+    public struct PartiallyUnarySupportedNumber
+    {
+        private readonly int number;
+
+        public PartiallyUnarySupportedNumber(int number)
+        {
+            this.number = number;
+        }
+
+        public static PartiallyUnarySupportedNumber operator ++(PartiallyUnarySupportedNumber me)
+        {
+            return new PartiallyUnarySupportedNumber(me.number + 1);
+        }
+
+        public static PartiallyUnarySupportedNumber operator ~(PartiallyUnarySupportedNumber me)
+        {
+            return new PartiallyUnarySupportedNumber(~me.number);
+        }
+    }
+}
---
+++ Money/Internal/Helpers/UnaryOperationHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Money.Internal.Helpers
+{
+    public static class UnaryOperationHelper
+    {
+        internal static bool SupportsUnaryOperation<T>(Func<Expression, UnaryExpression> unaryOperation)
+        {
+            try
+            {
+                var operand = Expression.Constant(default(T), typeof(T));
+                unaryOperation(operand);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool SupportsIncrement<T>()
+        {
+            return SupportsUnaryOperation<T>(Expression.Increment); 
+        }
+
+        public static bool SupportsDecrement<T>()
+        {
+            return SupportsUnaryOperation<T>(Expression.Decrement);
+        }
+
+        public static bool SupportsNegation<T>()
+        {
+            return SupportsUnaryOperation<T>(Expression.Negate);
+        }
+
+        public static bool SupportsUnaryPlus<T>()
+        {
+            return SupportsUnaryOperation<T>(Expression.UnaryPlus);
+        }
+
+        public static bool SupportsOnesCompliment<T>()
+        {
+            return SupportsUnaryOperation<T>(Expression.OnesComplement);
+        }
+    }
+}
