a2b7316f46c99f0b9d3b9a85b2de1c6ef5ec8bbb
zpbappi <zpbappi@gmail.com>
2016-03-08 14:56:14 +1100

    added new test for multicurrency operation return type

--- Money.Tests/MoneyTests/BinaryOperationWithMoney/BinaryOperationBetweenMoneyObjectsTest.cs
+++ Money.Tests/MoneyTests/BinaryOperationWithMoney/BinaryOperationBetweenMoneyObjectsTest.cs
@@ -49,5 +49,17 @@ namespace Money.Tests.MoneyTests.BinaryOperationWithMoney
             var actual = money1 % money2;
             actual.ShouldBeAssignableTo<Wallet>();
         }
+
+        [Fact]
+        public void AnyCombinationOfBinaryOperationsForMixOfCurrencies_ShouldReturnWallet()
+        {
+            var m1 = new Money<int>(42, "USD");
+            var m2 = new Money<float>(3.14159265f, "GBP");
+            var m3 = new Money<decimal>(1000000m, "AUD");
+            var m4 = new Money<long>(1234L, "EUR");
+
+            var actual = m1 + (m2*m3)/m4 - (m3%m4);
+            actual.ShouldBeAssignableTo<Wallet>();
+        }
     }
 }
